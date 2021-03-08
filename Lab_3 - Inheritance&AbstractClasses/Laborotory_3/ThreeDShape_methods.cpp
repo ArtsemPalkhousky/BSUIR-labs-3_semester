e6fa4dfbd6115fefa72b6d31d81c01bc88acc2cf
Artem <art.polhovsky>
2021-03-08 15:36:24 +0300

    C++

--- Lab_3 - Inheritance&AbstractClasses/Laborotory_3/ThreeDShape_methods.cpp
+++
@@ -1,9 +0,0 @@
-#include "ThreeDShape_Class.h"
-
-ThreeDshape ::ThreeDshape(string color){
-	cout << "Constructor 3D" << endl;
-	this->color = color;
-}
-ThreeDshape::~ThreeDshape(){
-	cout << "Destructor 3D" << endl;
-}
--- Lab_3 - Inheritance&AbstractClasses/Laborotory_3/TwoDShape_methods.cpp
+++
@@ -1,9 +0,0 @@
-#include "TwoDShape_Class.h"
-
-TwoDshape::TwoDshape(string color){
-	cout << "Constructor 2D" << endl;
-	this->color = color;
-}
-TwoDshape::~TwoDshape(){
-	cout << "Destructor 2D" << endl;
-}
